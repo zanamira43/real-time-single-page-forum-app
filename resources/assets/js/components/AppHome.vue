107c6027e47df68a2f04494fb0a9b7481343e53d
zana <zanamira43@gmail.com>
2018-03-16 08:32:50 +0300

    Vuetify, Vue Router, Initial Components

---
+++ resources/assets/js/components/AppHome.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<toolbar></toolbar>
+
+		 <v-container>
+			 <h1>hello</h1>
+		</v-container>
+
+		<app-footer></app-footer>
+	</div>
+	 
+</template>
+
+<script>
+import Toolbar from './Toolbar'
+import AppFooter from './AppFooter'
+
+export default {
+	name: 'app-home',
+	components: {Toolbar, AppFooter}
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+				<meta name="csrf-token" content="csrf_token">
+
+        <title>Single Forum App</title>
+
+        <!-- Cass -->
+        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+
+       
+    </head>
+    <body>
+       <div id="app">
+			 		<v-app>
+						<app-home></app-home>
+					</v-app>
+			 </div>
+
+
+			<script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
